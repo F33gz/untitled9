e5196485686600c9f10431b9a9dadc52c4b96779
fsegu <f.seguel03@ufromail.cl>
2024-10-09 15:51:00 -0300

    feature: recommendations anime api consumption added

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,16 @@
     <img v-if="randomManga" :src="randomManga.images.jpg.image_url" alt="Manga Image">
     <a v-if="randomManga" :href="randomManga.url" target="_blank">Ver en MyAnimeList</a>
     <p v-if="randomManga">{{ randomManga.synopsis }}</p>
+
+    <button @click="AnimeRecommendations">Recomendaciones de Anime</button>
+    <div v-if="animeRecommendations">
+      <div v-for="recommendation in animeRecommendations" :key="recommendation.entry.mal_id">
+        <h2>{{ recommendation.entry.title }}</h2>
+
+        <a :href="recommendation.entry.url" target="_blank">Ver en MyAnimeList</a>
+        <p>{{ recommendation.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,8 +34,7 @@ export default {
     return {
       randomAnime: null,
       randomManga: null,
-      animeByCountryAndYear: null,
-      mangaByCountryAndYear: null,
+      animeRecommendations: [],
     };
   },
   methods: {
@@ -43,6 +52,14 @@ export default {
         this.randomManga = response.data.data;
       } catch (error) {
         console.error("Error al obtener manga aleatorio:", error);
+      }
+    },
+    async AnimeRecommendations() {
+      try {
+        const response = await axios.get('https://api.jikan.moe/v4/recommendations/anime');
+        this.animeRecommendations = response.data.data;
+      } catch (error) {
+        console.error("Error al obtener recomendaciones de anime:", error);
       }
     },
   }
