192a840d2516134bea2cc936ccd8e1f904213410
fsegu <f.seguel03@ufromail.cl>
2024-10-09 15:29:40 -0300

    feature: random anime api consumption added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <div>
-    <h1>API</h1>
+    <h1>API Data Fetcher</h1>
 
     <button @click="RandomAnime">Anime Aleatorio</button>
     <p v-if="randomAnime">{{ randomAnime.title }}</p>
-    <img v-if="randomAnime" :src="randomAnime.images.jpg.image_url" alt="Anime Image">
+    <img v-if="randomAnime" :src="randomAnime.image_url" alt="Anime Image">
     <a v-if="randomAnime" :href="randomAnime.url" target="_blank">Ver en MyAnimeList</a>
     <p v-if="randomAnime">{{ randomAnime.synopsis }}</p>
 
-    <button @click="RandomManga">Manga Aleatorio</button>
-    <p v-if="randomManga">{{ randomManga.title }}</p>
-    <img v-if="randomManga" :src="randomManga.images.jpg.image_url" alt="Manga Image">
-    <a v-if="randomManga" :href="randomManga.url" target="_blank">Ver en MyAnimeList</a>
-    <p v-if="randomManga">{{ randomManga.synopsis }}</p>
-
     <div v-if="animeByCountryAndYear">
       <p>{{ animeByCountryAndYear.title }}</p>
-      <img :src="animeByCountryAndYear.images.jpg.image_url" alt="Anime Image">
+      <img :src="animeByCountryAndYear.image_url" alt="Anime Image">
       <a :href="animeByCountryAndYear.url" target="_blank">Ver en MyAnimeList</a>
       <p>{{ animeByCountryAndYear.synopsis }}</p>
-    </div>
-
-    <div v-if="mangaByCountryAndYear">
-      <p>{{ mangaByCountryAndYear.title }}</p>
-      <img :src="mangaByCountryAndYear.images.jpg.image_url" alt="Manga Image">
-      <a :href="mangaByCountryAndYear.url" target="_blank">Ver en MyAnimeList</a>
-      <p>{{ mangaByCountryAndYear.synopsis }}</p>
     </div>
   </div>
 </template>
@@ -38,8 +25,6 @@ export default {
     return {
       randomAnime: null,
       randomManga: null,
-      animeByCountryAndYear: null,
-      mangaByCountryAndYear: null,
     };
   },
   methods: {
@@ -54,31 +39,14 @@ export default {
     async RandomManga() {
       try {
         const response = await axios.get('https://api.jikan.moe/v4/random/manga');
-        this.randomManga = response.data.data;
+        this.randomManga = JSON.stringify(response.data.data);
       } catch (error) {
         console.error("Error al obtener manga aleatorio:", error);
       }
     },
-    async fetchAnimeByCountryAndYear(country, year) {
-      try {
-        const response = await axios.get(`https://api.jikan.moe/v4/anime?country=${country}&year=${year}`);
-        this.animeByCountryAndYear = response.data.data[0];
-      } catch (error) {
-        console.error("Error al obtener anime por país y año:", error);
-      }
-    },
-    async fetchMangaByCountryAndYear(country, year) {
-      try {
-        const response = await axios.get(`https://api.jikan.moe/v4/manga?country=${country}&year=${year}`);
-        this.mangaByCountryAndYear = response.data.data[0];
-      } catch (error) {
-        console.error("Error al obtener manga por país y año:", error);
-      }
-    }
   }
 };
 </script>
 
 <style>
-
 </style>
